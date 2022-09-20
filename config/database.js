ec46df877a35b84092d65335285175e1e7512e3d
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-09-20 18:15:05 -0500

    Update database connector to return promise

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGOOSE_DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    })
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    return conn.connection.getClient()
   } catch (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   }
 };
 
