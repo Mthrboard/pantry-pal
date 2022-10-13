4675cf3f0b54d0ff3ef29fc06a60109addbd2ef4
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-10-13 14:23:08 -0500

    Update validate to verify for consistency

--- routes/account.js
+++ routes/account.js
@@ -5,7 +5,7 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 //Main Routes - simplified for now
 router.get('/', ensureAuth, accountController.getAccount)
-router.get('/validateEmail/:userId/:token', accountController.getValidateEmail)
+router.get('/verifyEmail/:userId/:token', accountController.getVerifyEmail)
 router.get('/settings', ensureAuth, accountController.getSettings)
 
 module.exports = router
