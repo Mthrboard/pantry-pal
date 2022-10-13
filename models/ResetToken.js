1b439a63569d72e34ffce1e2ebf52ae3c9c9d27a
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-10-13 14:24:01 -0500

    Remove duplicate Token models and renamed files

--- models/ResetToken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ResetTokenSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  token: { type: String, required: true },
-  expireAt: { type: Date, expires: 3600 }
-});
-
-module.exports = mongoose.model("ResetToken", ResetTokenSchema);
