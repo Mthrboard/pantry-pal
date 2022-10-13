9a19462001b7363bbfe868378742075001a52eaa
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-10-13 14:21:51 -0500

    Update account to merge Token models into one

--- controllers/account.js
+++ controllers/account.js
@@ -1,5 +1,5 @@
 const User = require("../models/User")
-const ValidationToken = require("../models/ValidationToken")
+const Token = require("../models/Token")
 const { sendEmail } = require("../utils/sendEmail")
 const bcrypt = require("bcrypt")
 
@@ -7,17 +7,17 @@ module.exports = {
   getAccount: (req, res) => {
 
   },
-  getValidateEmail: async (req, res) => {
+  getVerifyEmail: async (req, res) => {
     try {
       console.log(req.params)
-      const token = await ValidationToken.findOne({ userId: req.params.userId })
+      const token = await Token.findOne({ userId: req.params.userId, type: 'emailVerification' })
       if (!token) throw new Error("Email validation token does not exist")
       const user = await User.findById({ _id: req.params.userId })
       if (!user) throw new Error("User does not exist")
       const isValidToken = await bcrypt.compare(req.params.token, token.token)
       if (!isValidToken) throw new Error("Invalid validation token")
-      await user.updateOne({ emailValidated: true })
-      sendEmail(user.email, "Email Validated Successfully", {name: user.name } , "validEmail.ejs")
+      await user.updateOne({ emailVerified: true })
+      sendEmail(user.email, "Email Verified Successfully", {name: user.name } , "verifiedEmail.ejs")
       await token.deleteOne()
       res.redirect("/dashboard")
     } catch (err) {
